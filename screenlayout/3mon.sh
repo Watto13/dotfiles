a6c2fdb966defcdbe57b6b4d5242a6aea5c5671b
Tom Watson <t.alexander.watson@gmail.com>
2024-05-30 15:23:42 +0100

    feat: Added screen layout script

---
+++ screenlayout/3mon.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI-0 --primary --mode 3840x2160 --pos 1080x1080 --rotate normal --output DP-0 --mode 1920x1080 --pos 2040x0 --rotate inverted --output DP-1 --off --output HDMI-1 --mode 1920x1080 --pos 0x1220 --rotate left --output DP-2 --off --output DP-3 --off --output None-1-1 --off
